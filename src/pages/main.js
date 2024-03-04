5dfc76edb5ed90929c9376f3e607076aef7f0f53
GUILHERME DILIO DE SOUZA <1091392413037@fatec-srv.local>
2024-03-04 20:33:53 -0300

    config: main class (04/03)

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,3 +1,14 @@
 import React, { Component } from "react";
 import {  Text, View } from "react-native";
 
+export default class Main extends Component {
+    render(){
+        return(
+            <View>
+                <Text>
+                    Olá, bem vindo a página principal.
+                </Text>
+            </View>
+        )
+    }
+}
